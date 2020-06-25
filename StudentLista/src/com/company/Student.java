7a4b29ec68db3fa7ee2747c534ab41d8a234274e
Vladan Cupric <47221439+VladanStar@users.noreply.github.com>
2020-06-25 14:17:17 +0200

    Add files via upload
    Kreirati klasu Student koja od atributa ima ime, prezime, broj indeksa, datum rođenja, godinu koju je poslednju upisao na fakultetu kao i niz ili listu (po izboru) koji broje maksimalno 32 ocene (student ne mora da ima 32 ocene). Ime i prezime treba biti tipa String, broj indeksa, godina na fakultetu i niz ocena treba biti tipa int, a datum rođenja može biti tipa String ili Date. Za sve attribute kreirati prazan i preopterećen konstruktor, getere, setere kao i toString metodu. Pored toga potrebno je implementirati i sledeće metode:
    •Metodu studentBrucos() koja vraća true ukoliko je student brucoš i false ukoliko nije
    • Metodu dodajPolozenIspit() koja kao argument prima ocenu i dodaje je u listu/niz ocena studenta.
    •Metodu racunajProsek() koja računa i vraća prosečnu ocenu studenta zaokruženu na dve decimale.
    U klasi Main kreirati proizvoljan niz studenata i za svakog prikazati da li je brucoš ili ukoliko nije njegovu prosečnu ocenu.

---
+++ StudentLista/src/com/company/Student.java
@@ -0,0 +1,109 @@
+package com.company;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class Student {
+    private String ime;
+    private String prezime;
+    private int brojIndeksa;
+    private int godinaStudiranja;
+    private String datumRodjenja;
+    private List<Integer> ocene = new ArrayList<>();
+
+    public Student(String ime, String prezime, int brojIndeksa, int godinaStudiranja, String datumRodjenja,) {
+        this.ime = ime;
+        this.prezime = prezime;
+        this.brojIndeksa = brojIndeksa;
+        this.godinaStudiranja = godinaStudiranja;
+        this.datumRodjenja = datumRodjenja;
+
+    }
+
+    public Student() {
+    }
+
+    public String getIme() {
+        return ime;
+    }
+
+    public void setIme(String ime) {
+        this.ime = ime;
+    }
+
+    public String getPrezime() {
+        return prezime;
+    }
+
+    public void setPrezime(String prezime) {
+        this.prezime = prezime;
+    }
+
+    public int getBrojIndeksa() {
+        return brojIndeksa;
+    }
+
+    public void setBrojIndeksa(int brojIndeksa) {
+        this.brojIndeksa = brojIndeksa;
+    }
+
+    public int getGodinaStudiranja() {
+        return godinaStudiranja;
+    }
+
+    public void setGodinaStudiranja(int godinaStudiranja) {
+        this.godinaStudiranja = godinaStudiranja;
+    }
+
+    public String getDatumRodjenja() {
+        return datumRodjenja;
+    }
+
+    public void setDatumRodjenja(String datumRodjenja) {
+        this.datumRodjenja = datumRodjenja;
+    }
+
+    public List<Integer> getOcene() {
+        return ocene;
+    }
+
+    public void setOcene(List<Integer> ocene) {
+        this.ocene = ocene;
+    }
+
+    public boolean brucosIliNe() {
+        return (this.godinaStudiranja == 1);
+    }
+
+    public double racunajProsek() {
+        int sum = 0;
+        for (int ocena : ocene) {
+            sum += ocena;
+        }
+        return (double) sum / ocene.size();
+    }
+
+    public void dodajPolozeniIspit(int ocena) {
+        if (ocene.size() < 32) {
+            if (ocena < 5 && ocena > 10) {
+                System.out.println("Ocena nije validna");
+            } else {
+                ocene.add(ocena);
+            }
+        } else {
+            System.out.println("Lista sadrzi maksimalan broj ocena");
+        }
+    }
+
+    @Override
+    public String toString() {
+        return "Student{" +
+                "ime='" + ime + '\'' +
+                ", prezime='" + prezime + '\'' +
+                ", brojIndeksa=" + brojIndeksa +
+                ", godinaStudiranja=" + godinaStudiranja +
+                ", datumRodjenja='" + datumRodjenja + '\'' +
+                ", ocene=" + ocene +
+                '}';
+    }
+}
